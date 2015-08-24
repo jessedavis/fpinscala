b3b08ee921e13cd2eaa726d1cdddeeb9da09bcae
jessedavis <jdavis@tracelytics.com>
2015-08-23 22:41:52 -0400

    [chapter3] work on Tree data structure, should still study/implement fold example

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,32 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  // didn't get the additional + 1 for the Branch case
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1 
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(x) => x
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
+  // didn't get Leaf having a depth of 0 either
+  // makes sense though, need to count the depth on the branch
+  // since you'll be descending/traversing it to get to the next
+  // possible depth
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) = Branch(map(l)(f), map(r)(f)) 
+  }
+
+  // exercise 3.29
+  // didn't get fold, but should have looked up its signature to start with
+  // look it up in master
 }
